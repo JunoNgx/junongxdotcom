5431591f40405b34e79eccea90e62e2a64793fa0
JunoNgx <juno.ngx@gmail.com>
2020-08-29 17:02:31 +0800

    Update contact URLs

--- components/URL.js
+++ components/URL.js
@@ -1,10 +1,10 @@
 export const URL = {
     twitter: "https://twitter.com/JunoNgx",
-    itchio: "https://junongx.itch.io/",
+    itchio: "https://JunoNgx.itch.io/",
     github: "https://github.com/JunoNgx",
     email: "mailto:juno.ngx@gmail.com",
     instagram: "https://instagram.com/scientistxprincess/",
-    flickr: "https://flickr.com/people/junongx/",
-    linkedin: "https://linkedin.com/in/minh-tri-nguyen-a211a217a/",
+    flickr: "https://flickr.com/people/JunoNgx/",
+    linkedin: "https://www.linkedin.com/in/JunoNgx/",
     ebonymemo: "http://ebonymemo.com/"
 }
