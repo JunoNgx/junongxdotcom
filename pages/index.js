11100cc9ccbb80d4858d7da5286eac825e905d22
JunoNgx <juno.ngx@gmail.com>
2020-08-30 11:53:53 +0800

    Add meta description tag

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
                 <title>Juno Nguyen</title>
                 <meta content={"Juno Nguyen"} key="title"/>
                 <meta name="theme-color" content="#222"/>
+				<meta name="description" content="Singapore-based software developer, web developer, game developer, and artist"/>
             </Head>
             <div className="page">
                 {/* <div className="page--links"> */}
