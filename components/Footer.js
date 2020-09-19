74383764639e462b0d8e5166b2662c14486fe6ce
JunoNgx <juno.ngx@gmail.com>
2020-09-19 10:53:42 +0800

    Update content and add footer source note

--- components/Footer.js
+++ components/Footer.js
@@ -5,6 +5,7 @@ export default function Footer() {
     return (
         <div className="footer">
             <p>made by Juno Nguyen with <CA link="https://nextjs.org/">NextJS</CA></p>
+            <p>this site is uncopyrighted and <CA link="https://github.com/JunoNgx/junongxdotcom">open source</CA></p>
         </div>
     )
 }
