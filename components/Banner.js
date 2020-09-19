7fa50855e8caa58daf4cbd4ac39441533c917f76
JunoNgx <juno.ngx@gmail.com>
2020-09-20 01:22:54 +0800

    Content update

--- components/Banner.js
+++ components/Banner.js
@@ -6,9 +6,9 @@ export default function Banner() {
     return (
         <div className="section section--banner">
             <h1 className="section__brand">Hi, I'm Juno Nguyen.</h1>
-            <p>I am a <strong>software developer</strong> based in Singapore, best known as the creator of the website <CA link={URL.ebonymemo}>Ebony Memo</CA> and <strong>the independent videogame developer</strong> <CA link={URL.itchio}>manafarm</CA>.</p>
+            <p>I am a software developer based in Singapore, best known as the creator of the website <CA link={URL.ebonymemo}>Ebony Memo</CA> and the independent videogame developer <CA link={URL.itchio}>manafarm</CA>.</p>
             
-            <p>I am a <strong>generalist</strong>, having worked on videogames, full-stack web applications, command tools and still exploring. Most of my works are free and open source and can be found on <CA link={URL.github}>my GitHub</CA>, with the notable ones being listed below.</p>
+            <p>I am a generalist, having worked on videogames, full-stack web applications, command line tools, and still exploring. Most of my works are free and open source and can be found on <CA link={URL.github}>my GitHub</CA>. The notable ones are also listed below.</p>
         </div>
     )
 }
