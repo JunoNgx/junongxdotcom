ce3a39f674d002481c7c9f6413676c5ca8cd2be6
JunoNgx <juno.ngx@gmail.com>
2020-09-19 10:25:38 +0800

    Add github url to banner

--- components/Banner.js
+++ components/Banner.js
@@ -8,7 +8,7 @@ export default function Banner() {
             <h1 className="section__brand">Hi, I'm Juno Nguyen.</h1>
             <p>I am a <strong>software developer</strong> based in Singapore, best known as the creator of the website <CA link={URL.ebonymemo}>Ebony Memo</CA> and <strong>the independent videogame developer</strong> <CA link={URL.itchio}>manafarm</CA>.</p>
             
-            <p>I am a <strong>generalist</strong>, having worked on videogames, full-stack web applications, command tools and still exploring. Most of my works are free and open source and can be found on my GitHub, with the notable ones being listed below.</p>
+            <p>I am a <strong>generalist</strong>, having worked on videogames, full-stack web applications, command tools and still exploring. Most of my works are free and open source and can be found on <CA link={URL.github}>my GitHub</CA>, with the notable ones being listed below.</p>
         </div>
     )
 }
