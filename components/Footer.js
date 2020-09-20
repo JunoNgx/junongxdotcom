fcd1467609aa840e2a3621a893970a9cf2ea2d16
JunoNgx <juno.ngx@gmail.com>
2020-09-20 12:26:29 +0800

    Update footer content

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ import CA from './CustomAnchor'
 export default function Footer() {
     return (
         <div className="footer">
-            <p>made by Juno Nguyen with <CA link="https://nextjs.org/">NextJS</CA></p>
-            <p>this site is uncopyrighted and <CA link="https://github.com/JunoNgx/junongxdotcom">open source</CA></p>
+            <p>made with <CA link="https://nextjs.org/">NextJS</CA> and <CA link="https://sass-lang.com/">SASS</CA></p>
+            <p>uncopyrighted and <CA link="https://github.com/JunoNgx/junongxdotcom">open source</CA></p>
         </div>
     )
 }
