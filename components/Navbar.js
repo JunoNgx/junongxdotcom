53c21db34ee6522efe21f923b0d25d5012dbe47e
JunoNgx <juno.ngx@gmail.com>
2020-09-22 01:15:01 +0800

    Revert back to CSS transition due to complications

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,52 +1,30 @@
 import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
 import { URL } from './URL'
 import CA from './CustomAnchor'
-import { useMediaQuery } from 'react-responsive'
 
 export default function Navbar() {
 
     const [isExpanded, setIsExpanded] = useState(false)
-    const isMobile = useMediaQuery({ maxDeviceWidth: 720 })
 
-    // Expand animation is powered by react-spring
-    const springProps = useSpring({
-        fontSize: isExpanded ? '2rem' : '1.2rem',
-        transform: isExpanded ? 'rotate(225deg)' : 'rotate(45deg)',
-        transformOrigin: isExpanded ? '65% 65%' : '100% 100%',
-        clipPath: isExpanded ? 'circle(1000px at 90% 5%)' : 'circle(0px at 90% 5%)',
+    let navbarControlClass = "navbar__control"
+    let navbarControlIconClass = "navbar__control__icon"
+    let navbarControlLabelClass = "navbar__control__label"
+    let navbarContentClass = "navbar__content"
 
-        config: {
-            duration: 400,
-            // easeOutQuart
-            easing: t => 1-(--t)*t*t*t
-        }
-    })
-
-    // react-spring and animation are only used in mobile layout
-    const navbarContentStyle = (isMobile)
-        ? {clipPath: springProps.clipPath}
-        : {}
+    if (isExpanded) {
+        navbarControlClass += " " + navbarControlClass + "--expanded"
+        navbarControlIconClass += " " + navbarControlIconClass + "--expanded"
+        navbarControlLabelClass += " " + navbarControlLabelClass + "--expanded"
+        navbarContentClass += " " + navbarContentClass + "--expanded"
+    }
 
     return (
         <div className="navbar">
-            <div
-            className={"navbar__control"}
-            onClick={()=>setIsExpanded(isExpanded=>!isExpanded)}>
-                <animated.p
-                className={"navbar__control__label"}
-                style={{fontSize: springProps.fontSize}}>
-                    My contacts
-                </animated.p>
-                <animated.div
-                className={"navbar__control__icon"}
-                style={{transform: springProps.transform, transformOrigin: springProps.transformOrigin}}
-                />
+            <div className={navbarControlClass} onClick={()=>setIsExpanded(isExpanded=>!isExpanded)}>
+                <p className={navbarControlLabelClass}>My contacts</p>
+                <div className={navbarControlIconClass}/>
             </div>
-            <animated.div
-            className={"navbar__content"}
-            style={navbarContentStyle}
-            >
+            <div className={navbarContentClass}>
                 <ul className="navbar__content__contacts">
                     <ContactLink link={URL.twitter}>Twitter</ContactLink>
                     <ContactLink link={URL.github}>GitHub</ContactLink>
@@ -56,12 +34,10 @@ export default function Navbar() {
                     <ContactLink link={URL.email}>Email</ContactLink>
                     <ContactLink link={URL.linkedin}>LinkedIn</ContactLink>
                 </ul>
-                <p
-                className="navbar__content__contacts__close"
-                onClick={()=>{setIsExpanded(false)}}>
+                <p className="navbar__content__contacts__close" onClick={()=>{setIsExpanded(false)}}>
                     ╳
                 </p>
-            </animated.div>
+            </div>
         </div>
 
     )
