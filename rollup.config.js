3da0c222ae29cd4c7d64c6a1092d2615a7df8ed2
Juno Nguyen <juno.ngx@gmail.com>
2021-07-28 21:51:15 +0800

    Reconfigure rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import preprocess from 'svelte-preprocess';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -37,16 +38,13 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.css');
+        svelte ({
+            compilerOptions: {
+                dev: !production,
             },
             preprocess: preprocess()
-		}),
+        }),
+        css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
