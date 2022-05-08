c5cec417e0b62ffe51258037fe31944765ecd7ea
Juno Nguyen <juno.ngx@gmail.com>
2022-05-08 20:26:50 +0800

    Tune footer

--- src/lib/BaseFooter.svelte
+++ src/lib/BaseFooter.svelte
@@ -3,20 +3,19 @@
 </script>
 
 <template lang="pug">
-    div(class="footer")
+    footer
         p made with 
             CA(link="https://svelte.dev/") svelte
             |  and 
             CA(link="https://sass-lang.com/") sass
-        p uncopyrighted and
-            CA(link="https://github.com/JunoNgx/junongxdotcom") 
-            |  open source
+        p uncopyrighted and 
+            CA(link="https://github.com/JunoNgx/junongxdotcom") open source
 </template>
 
 <style lang="sass">
     @use "../styles/vars" as v
 
-    .footer 
+    footer
         font-size: v.$font-size
         margin: 5rem auto 1rem auto
         text-align: center
