9e9f0b6a8067ad9ede2b4fffaf63d9116add3379
Juno Nguyen <juno.ngx@gmail.com>
2022-05-08 18:18:57 +0800

    Install marked for markdown syntax

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-    export let entry: Entry;
+    import { marked } from 'marked'
+
+    export let entry: Entry
 </script>
 
 <template lang="pug">
     div(class="section")
         h3 {entry.title}
-        p(class="section__summary") {entry.summary}
-        p(class="section__content") {entry.content}
+        p(class="section__summary") {@html marked(entry.summary)}
+        p(class="section__content") {@html marked(entry.content)}
         +each('entry.links as link')
             a(href!="{link.url}") {link.label}
 
