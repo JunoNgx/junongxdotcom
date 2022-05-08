97203e86481c8db5c0b4d4e09b42cafdd745adc7
Juno Nguyen <juno.ngx@gmail.com>
2022-05-08 18:33:38 +0800

    Add style to Section component

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -6,7 +6,8 @@
 
 <template lang="pug">
     div(class="section")
-        h3 {entry.title}
+        h3(class="section__title") {entry.title}
+        img(class="section__banner" src!="{entry.imgSrc}" alt!="{entry.imgAlt}")
         p(class="section__summary") {@html marked(entry.summary)}
         p(class="section__content") {@html marked(entry.content)}
         +each('entry.links as link')
@@ -15,5 +16,20 @@
 </template>
 
 <style lang="sass">
+    .section
+        margin: 2rem
+        padding: 1rem
+        max-width: 400px
+        border: 1px solid #444
+        text-align: left
+        hyphens: auto
 
+        &__banner
+            display: block
+            margin: auto
+            max-width: 100%
+
+        &__title
+            margin-top: 0
+            font-size: 2rem
 </style>
