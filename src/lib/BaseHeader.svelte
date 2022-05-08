11e77325f05dea56436d6e28df2be1df7ee400e3
Juno Nguyen <juno.ngx@gmail.com>
2022-05-09 00:34:53 +0800

    Tune header

--- src/lib/BaseHeader.svelte
+++ src/lib/BaseHeader.svelte
@@ -10,12 +10,22 @@
 
 <style lang="sass">
     @use "../styles/_mixins" as m
+    @use "../styles/vars" as v
 
     header
         text-align: right
 
         h1
             margin-top: 0
+            text-align: center
+            color: v.$col-bg
+            background-color: v.$col-acc
+            // +m.transition(padding-right, color, background-color)
+
+            // &:hover
+                // padding-right: 3rem
+                // color: v.$col-bg
+                // background-color: v.$col-acc
 
         +m.mobile
             text-align: left
