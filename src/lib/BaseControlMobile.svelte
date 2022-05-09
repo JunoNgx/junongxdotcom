fb67012c537e531d68afd8c119b81477e9f63203
Juno Nguyen <juno.ngx@gmail.com>
2022-05-10 03:14:06 +0800

    Tune dark theme for control mobile

--- src/lib/BaseControlMobile.svelte
+++ src/lib/BaseControlMobile.svelte
@@ -27,42 +27,49 @@
     @use '../styles/mixins' as m
 
     .control-mobile
-        &__icon-container
-            z-index: 1
-            position: fixed
-            top: 1.5rem
-            right: 1.5rem
-            width: 48px
-            height: 48px
-            color: v.$col-bg
-            background-color: v.$col-acc
-            cursor: pointer
+        display: none
 
-            display: flex
-            justify-content: space-around
-            align-items: center
+        +m.mobile
+            display: block
 
-        &__content
-            position: fixed
-            top: 0
-            right: 0
-            height: 100vh
-            width: 100vw
-            background-color: v.$col-acc
-            clip-path: circle(0 at calc(100% - 2.5rem) calc(2.5rem))
-            transition: clip-path v.$trans-time-default ease-out
+            &__icon-container
+                z-index: 1
+                position: fixed
+                top: 1.5rem
+                right: 1.5rem
+                width: 48px
+                height: 48px
+                color: v.$col-pri
+                background-color: v.$col-acc
+                cursor: pointer
+                user-select: none
 
-            &__control-wrapper
-                padding: 3rem 2rem 1rem
+                display: flex
+                justify-content: space-around
+                align-items: center
 
-            &--is-expanded
-                clip-path: circle(90vh at calc(100% - 2.5rem) calc(2.5rem))
+            &__content
+                position: fixed
+                top: 0
+                right: 0
+                height: 100vh
+                width: 100vw
+                background-color: v.$col-acc
+                clip-path: circle(0 at calc(100% - 2.5rem) calc(2.5rem))
+                transition: clip-path v.$trans-time-default ease-out
 
-        &--dark
-            .control-mobile__icon-container
-                background-color: v.$col-acc-dark
-            
-            .control-mobile__content
-                background-color: v.$col-acc-dark
+                &__control-wrapper
+                    padding: 3rem 2rem 1rem
+
+                &--is-expanded
+                    clip-path: circle(90vh at calc(100% - 2.5rem) calc(2.5rem))
+
+            &--dark
+                .control-mobile__icon-container
+                    color: v.$col-pri-dark
+                    background-color: v.$col-acc-dark
+                
+                .control-mobile__content
+                    background-color: v.$col-acc-dark
 
 </style>
