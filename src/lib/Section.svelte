29bd9bba24747aff06cf342cc316bfc66b9ff185
Juno Nguyen <juno.ngx@gmail.com>
2023-07-09 00:43:45 +0800

    #7: section: rename links and buttons

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -32,25 +32,25 @@
 
         p.section__content(class!="{isExpanded ? 'section__content--is-expanded' : 'section__content--is-collapsed'}") {@html marked(entry.content)}
 
-        .section__buttons
-            .section__buttons__links
+        .section__buttons-container
+            .section__links-container
                 +each('entry.links as link')
-                    a.section__buttons__links__item(href!="{link.url}" rel="noopener noreferrer" target="_blank")
+                    a.section__link-item(href!="{link.url}" rel="noopener noreferrer" target="_blank")
                         span {link.label}
                         +if("link.label === 'source'")
                             // External link
-                            svg.section__buttons__links__item__icon(xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round")
+                            svg.section__link-icon(xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round")
                                 polyline(points="16 18 22 12 16 6")
                                 polyline(points="8 6 2 12 8 18")
                             +else()
                                 // Code
-                                svg.section__buttons__links__item__icon(xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round")
+                                svg.section__link-icon(xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round")
                                     path(d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6")
                                     polyline(points="15 3 21 3 21 9")
                                     line(x1="10" y1="14" x2="21" y2="3")
-            button.section__buttons__expand-btn(on:click!="{handleExpandClick}")
+            button.section__expand-button(on:click!="{handleExpandClick}")
                 +if('isExpanded')
-                    span.section__buttons__expand-btn__label Less
+                    span.section__expand-button-label Less
                     +else()
                         span More
 
@@ -110,65 +110,65 @@
                 +m.mobile
                     font-size: v.$font-size-mobile
 
-        &__buttons
+        &__buttons-container
             display: flex
             flex-flow: row nowrap
             justify-content: space-between
             align-items: center
             gap: 1rem
 
-            &__links
-                display: flex
-                flex-flow: row wrap
-                justify-content: space-around
-                align-items: center
-                gap: 0.2rem
-                flex-grow: 4
+        &__links-container
+            display: flex
+            flex-flow: row wrap
+            justify-content: space-around
+            align-items: center
+            gap: 0.2rem
+            flex-grow: 4
 
-                &__item
-                    padding: 0.25rem 0.5rem
-                    background-image: none
-                    background-color: v.$col-acc
-                    font-family: "Noto Sans Mono", monospace
-                    font-size: 12px
+        &__link-item
+            padding: 0.25rem 0.5rem
+            background-image: none
+            background-color: v.$col-acc
+            font-family: "Noto Sans Mono", monospace
+            font-size: 12px
 
-                    display: flex
-                    flex-flow: row nowrap
-                    justify-content: space-around
-                    align-items: center
-                    gap: 0.5rem
-                    +m.transition(background-color, color, box-shadow)
+            display: flex
+            flex-flow: row nowrap
+            justify-content: space-around
+            align-items: center
+            gap: 0.5rem
+            +m.transition(background-color, color, box-shadow)
 
-                    &:hover
-                        color: v.$col-bg
-                        background-color: v.$col-acc2
-                        box-shadow: 4px 4px v.$col-pri
+            &:hover
+                color: v.$col-bg
+                background-color: v.$col-acc2
+                box-shadow: 4px 4px v.$col-pri
 
-                    .section--is-dark &
-                        background-color: v.$col-acc-dark
-                        &:hover
-                            color: v.$col-bg-dark
-                            background-color: v.$col-acc2
-                            box-shadow: 4px 4px v.$col-pri-dark
+            .section--is-dark &
+                background-color: v.$col-acc-dark
+                &:hover
+                    color: v.$col-bg-dark
+                    background-color: v.$col-acc2
+                    box-shadow: 4px 4px v.$col-pri-dark
 
-            &__expand-btn
-                // border: 1px solid v.$col-pri
-                // border-radius: 0
-                // padding: 0.25rem 0.5rem
-                // cursor: pointer
-                // color: v.$col-pri
-                // background-color: transparent
-                // font-family: v.$fonts
-                // font-size: v.$font-size
-                // +m.transition(color, background-color)
+        &__expand-button
+            // border: 1px solid v.$col-pri
+            // border-radius: 0
+            // padding: 0.25rem 0.5rem
+            // cursor: pointer
+            // color: v.$col-pri
+            // background-color: transparent
+            // font-family: v.$fonts
+            // font-size: v.$font-size
+            // +m.transition(color, background-color)
 
-                // &:hover
-                //     color: v.$col-bg
-                //     background-color: v.$col-pri
-                +m.button
+            // &:hover
+            //     color: v.$col-bg
+            //     background-color: v.$col-pri
+            +m.button
 
-                .section--is-dark &
-                    +m.button-dark
+            .section--is-dark &
+                +m.button-dark
 
         +m.mobile
             margin: 2rem 0.5rem
