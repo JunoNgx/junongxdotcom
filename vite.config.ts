096295814ad2a890c4655208e2a9592f04db5555
Juno Nguyen <juno.ngx@gmail.com>
2023-07-15 16:14:58 +0800

    Use vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import yaml from '@rollup/plugin-yaml';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         svelte(),
-        yaml()
-    ]
+        yaml(),
+        tsconfigPaths(),
+    ],
 })
