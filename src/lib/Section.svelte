f260d4ac1790215f07227e8b73699079634c7e9c
Juno Nguyen <juno.ngx@gmail.com>
2024-06-01 00:58:52 +0800

    #30: change link icon

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -79,9 +79,8 @@
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
                             >
-                                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-                                <polyline points="15 3 21 3 21 9" />
-                                <line x1="10" y1="14" x2="21" y2="3" />
+                                <line x1="7" y1="17" x2="17" y2="7"></line>
+                                <polyline points="7 7 17 7 17 17"></polyline>
                             </svg>
                         {/if}
                     </a>
