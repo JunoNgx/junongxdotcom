725efa7a92cc485ca030355f1e601b536c71ae70
Juno Nguyen <juno.ngx@gmail.com>
2026-04-07 21:38:02 +0800

    feat: remove ts-config-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import yaml from '@rollup/plugin-yaml';
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         svelte(),
         yaml(),
-        tsconfigPaths(),
     ],
+    resolve: {
+        tsconfigPaths: true,
+    },
 })
